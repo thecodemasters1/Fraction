abe0119ff4de5bcbfc66c1ea7abf21ece765671d
Lahav Schlesinger <itslahav@gmail.com>
2015-08-22 20:01:47 +0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "Fraction.hpp"
 #include <iostream>
-#include <vector>
-#include <memory>
 
 int main() {
-	fraction::Fraction frac(2,3);
-	fraction::Fraction frac2(2,4);
-	std::cout << (frac+frac2) << std::endl;
-
+	//int x = 46342 * 46340;
+	//std::cout << x << std::endl;
+	fraction::Fraction frac;
+	std::cin >> frac;
+	std::cout << "frac= " << frac << std::endl;
+	/*
+	try {
+		fraction::Fraction frac(1073741823, 1, true);
+		fraction::Fraction frac2(1073741824, 1, true);
+		std::cout << (frac + frac2) << std::endl;
+	}
+	catch (...) {
+		std::cout << "HERE" << std::endl;
+	}
+	*/
 	//std::cout << "frac = " << frac << std::endl;
 }
