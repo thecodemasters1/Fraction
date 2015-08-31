12e0a1dbd66ba4185dd49c38c97a61c19dbd01cf
Lahav Schlesinger <itslahav@gmail.com>
2015-08-31 22:18:31 +0300

    added general abstract numeric exception

---
+++ NumericException.cpp
@@ -0,0 +1,28 @@
+/**
+* Author: Lahav Schlesinger
+**/
+
+/*
+* In this we have the implementation of the NumericException's destructor
+*/
+
+#include "NumericException.hpp"
+
+
+/***
+* NumericException::~NumericException() - The destructor
+*
+*Purpose:
+*       To exist.
+*
+*       we separate the definition of the pure virtual destructor from its
+*       declaration in the .cpp file.
+*
+*Entry:
+*
+*Exit:
+*
+*Exceptions:
+*
+*******************************************************************************/
+NumericException::~NumericException() { }
---
+++ NumericException.hpp
@@ -0,0 +1,32 @@
+#ifndef NUMERICEXCEPTION_HPP_
+#define NUMERICEXCEPTION_HPP_
+
+#include <exception>
+#include <string>
+
+/*
+This class represents a generic numeric exception, to which other numeric exceptions
+are to be derived from.
+Note that this class is abstract.
+*/
+class NumericException : public std::exception
+{
+public:
+	//-- constructors/destructor --//
+
+	/* constructor */
+
+	//Returns the error message as a C string.
+	virtual const char* what() const noexcept { return this->m_err_msg.c_str(); }
+
+	/* destructor */
+	virtual ~NumericException() = 0;
+protected:
+
+	//-- protected data members --//
+
+	//The error message
+	std::string m_err_msg;
+};
+
+#endif
