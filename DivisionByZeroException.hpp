6cebb437ad82ee7728fe68f39a9febb1fde5ee6f
Lahav Schlesinger <itslahav@gmail.com>
2015-08-31 22:21:32 +0300

    made to inherite from NumericException

--- DivisionByZeroException.hpp
+++ DivisionByZeroException.hpp
@@ -1,15 +1,33 @@
+/**
+* Author: Lahav Schlesinger
+**/
+
+/*
+* In this file we have the declaration of the DivisionByZeroException class
+*/
+
+
 #ifndef DIVISIONBYZEROEXCEPTION_HPP_
 #define DIVISIONBYZEROEXCEPTION_HPP_
 
-#include <exception>
+#include "NumericException.hpp"
 
-class DivisionByZeroException : public std::exception
+
+/*
+This class represents an exception class that should be thrown whenever a
+division by 0 occured.
+*/
+class DivisionByZeroException : public NumericException
 {
 public:
-	DivisionByZeroException();
+	//-- constructors/destructor --//
 
-	virtual const char* what() const noexcept; 
-	~DivisionByZeroException();
+	/* constructor */
+
+	//Sets the error message to "error dividing by 0".
+	DivisionByZeroException() {
+		this->m_err_msg = "error dividing by 0";
+	}
 };
 
 #endif
