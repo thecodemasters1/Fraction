1767eee1fc06207e5a6a885fe47cd9f2bce38336
Lahav Schlesinger <itslahav@gmail.com>
2015-08-31 22:21:50 +0300

    made to inherite from NumericException

--- NumericOverflowException.hpp
+++ NumericOverflowException.hpp
@@ -1,15 +1,33 @@
+/**
+* Author: Lahav Schlesinger
+**/
+
+/*
+* In this file we have the declaration of the DivisionByZeroException class
+*/
+
+
 #ifndef NUMERICOVERFLOWEXCEPTION_HPP_
 #define NUMERICOVERFLOWEXCEPTION_HPP_
 
-#include <exception>
+#include "NumericException.hpp"
 
-class NumericOverflowException : public std::exception
+
+/*
+This class represents an exception class that should be thrown whenever a
+numeric overflow occurs.
+*/
+class NumericOverflowException : public NumericException
 {
 public:
-	NumericOverflowException() {}
+	//-- constructors/destructor --//
 
-	virtual const char* what() const noexcept { return NULL; }
-	~NumericOverflowException() {}
+	/* constructor */
+
+	//Sets the error message to "numeric overflow detected".
+	NumericOverflowException() {
+		this->m_err_msg = "numeric overflow detected";
+	}
 };
 
 #endif
