04e88117a2b099658aeead5a013a5394cce9c8e5
Lahav Schlesinger <itslahav@gmail.com>
2015-08-31 22:19:29 +0300

    changed exmaple

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include "Fraction.hpp"
+#include "NumericException.hpp"
 #include <iostream>
+#include <climits>
 
 int main() {
-	//int x = 46342 * 46340;
-	//std::cout << x << std::endl;
-	fraction::Fraction frac;
-	std::cin >> frac;
-	std::cout << "frac= " << frac << std::endl;
-	/*
+
 	try {
-		fraction::Fraction frac(1073741823, 1, true);
-		fraction::Fraction frac2(1073741824, 1, true);
+		fraction::Fraction frac((INT_MAX / 2) + 1, 1);
+		fraction::Fraction frac2((INT_MAX / 2) + 1, 1, true);
 		std::cout << (frac + frac2) << std::endl;
 	}
-	catch (...) {
-		std::cout << "HERE" << std::endl;
+	catch (NumericException& err) {
+		std::cout << err.what() << std::endl;
 	}
-	*/
-	//std::cout << "frac = " << frac << std::endl;
 }
